0e393c3b4585a772cc69d4f2b0d7f5a12c5f2fd9
范轶洁 <fanyjb@digitalchina.com>
2020-02-25 21:48:06 +0800

    emailSendConfig

---
+++ middlewares/wuhan.js
@@ -0,0 +1,16 @@
+// login.js
+module.exports = (req, res, next) => {
+  if (req.method == "GET" && req.path == "/emailSendConfig") {
+    res.status(200).json({
+      smtpFromEmail: "dcgmail@126.com",
+      smtpFromUserName: "邮件与订阅服务",
+      smtpHost: "smtp.126.com",
+      smtpPassword: "dcg2020",
+      smtpPost: 25,
+      smtpSslEnable: true,
+      smtpUserName: "dcgmail@126.com"
+    });
+  } else {
+    next();
+  }
+};
